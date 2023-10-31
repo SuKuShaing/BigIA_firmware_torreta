01cd97acede9e3d0a99daf89586c2ffd8bccd8b7
Sebastian <sebastian.sanhueza.t@usach.cl>
2023-10-31 17:50:32 -0300

    Se añadió comentarios a la programación del motor

--- FrontEnd_py/tools_web.py
+++ FrontEnd_py/tools_web.py
@@ -10,30 +10,31 @@ class Stepper():
         self.nombre = nombre
         self.angulo = None
         self.id = id
-        self.ROT = 1 # default CW
-        self.delay = 0.0000608 / 32
+        self.ROT = 1  # default CW (sentido de giro por defecto, en el sentido de las agujas del reloj)
+        self.delay = 0.0000608 / 32  # Tiempo de retardo para el motor (puedes ajustarlo según sea necesario)
 
+        # Configuración de los pines DIR (dirección) y STEP (paso) según el ID del motor
         if id == 1:
-            self.DIR = 20
-            self.STEP = 21
-
+            self.DIR = 20  # Número de pin GPIO para la dirección del motor 1
+            self.STEP = 21  # Número de pin GPIO para el paso del motor 1
         elif id == 2:
-            self.DIR = 19
-            self.STEP = 26
-            
-    def mover_stepper(self, sentido, pasos):
+            self.DIR = 19  # Número de pin GPIO para la dirección del motor 2
+            self.STEP = 26  # Número de pin GPIO para el paso del motor 2
 
+    def mover_stepper(self, sentido, pasos):
+        # Configura el sentido de giro del motor basado en la entrada del usuario ('CW' para sentido de las agujas del reloj y 'CCW' para sentido contrario)
         if sentido == 'CW':
-            self.ROT = 1
-            print(" {}, DIR : {}, ROT : {}".format(self.nombre, self.DIR, self.ROT))
-            GPIO.output(self.DIR, 1)
+            self.ROT = 1  # Establece el sentido de rotación como CW (sentido de las agujas del reloj)
+            print(" {}, DIR : {}, ROT : {}".format(self.nombre, self.DIR, self.ROT))  # Imprime información del motor
+            GPIO.output(self.DIR, 1)  # Configura el pin de dirección para el sentido de las agujas del reloj
         elif sentido == 'CCW':
-            self.ROT = 0
-            print(" {}, DIR : {}, ROT : {}".format(self.nombre, self.DIR, self.ROT))
-            GPIO.output(self.DIR, 0)
-        
+            self.ROT = 0  # Establece el sentido de rotación como CCW (sentido contrario a las agujas del reloj)
+            print(" {}, DIR : {}, ROT : {}".format(self.nombre, self.DIR, self.ROT))  # Imprime información del motor
+            GPIO.output(self.DIR, 0)  # Configura el pin de dirección para el sentido contrario a las agujas del reloj
+
+        # Genera el número de pasos especificados
         for x in range(pasos):
-            GPIO.output(self.STEP, GPIO.HIGH)
-            sleep(self.delay)
-            GPIO.output(self.STEP, GPIO.LOW)
-            sleep(self.delay)
+            GPIO.output(self.STEP, GPIO.HIGH)  # Establece el pin de paso en alto
+            sleep(self.delay)  # Espera el tiempo definido
+            GPIO.output(self.STEP, GPIO.LOW)  # Establece el pin de paso en bajo
+            sleep(self.delay)  # Espera el tiempo definido
