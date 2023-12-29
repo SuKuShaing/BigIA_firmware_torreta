55d9247ef3c431f57ac8c9315db1e85738c8fdc9
Sebastian <sebastian.sanhueza.t@usach.cl>
2023-12-29 19:59:10 -0300

    se modifica el host para que se ejecute en el que trae por defecto

--- FrontEnd_py/server.py
+++ FrontEnd_py/server.py
@@ -66,5 +66,6 @@ def fuego():
 
 
 if __name__ == '__main__':
-    app.run(port = 1313, debug=True, host="0.0.0.0")  #app.run se encarga de ejecuar el servidor, por defecto en el puerto 5000, y el host por defecto es 127.0.0.1, La aplicación está en modo de prueba, gracias a esta linea cada vez que cambio algo se reinicia el servidor
+    # app.run(port = 1313, debug=True, host="0.0.0.0")  #app.run se encarga de ejecutar el servidor, por defecto en el puerto 5000, y el host por defecto es 127.0.0.1, La aplicación está en modo de prueba, gracias a esta linea cada vez que cambio algo se reinicia el servidor
+    app.run(port = 1313)  #app.run se encarga de ejecutar el servidor, por defecto en el puerto 5000, y el host por defecto es 127.0.0.1, La aplicación está en modo de prueba, gracias a esta linea cada vez que cambio algo se reinicia el servidor
                                                         #los puertos 0 al 1024 están ocupados por el mismo pc, del 1025 al 65536 (2exp16)
